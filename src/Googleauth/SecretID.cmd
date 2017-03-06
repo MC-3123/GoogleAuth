b7a4cc40fb7eb49c8dea14e625cfc1c5c2278a21
MC-3123 <mattcullen52@yahoo.com.au>
2017-03-06 22:45:50 +1100

    Add project files.

---
+++ src/Googleauth/SecretID.cmd
@@ -0,0 +1,4 @@
+dotnet user-secrets set Authentication:Google:ClientID 725620859851-nt88b0k0dg64s9t8d7pd41kuajnt45jn.apps.googleusercontent.com
+dotnet user-secrets set Authentication:Google:ClientSecret GprShUkSiS0wUgaOmTXxloHF
+dotnet ef database update
+pause
